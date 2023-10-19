de6241d61aa8e6989c59a4f6f5e53b11728fa8fc
Chris-xun <yx20021225@163.com>
2023-10-19 16:53:28 +0100

    update

--- PS3/6.py
+++ PS3/6.py
@@ -12,9 +12,8 @@ def DFT(f, t):
     # for p in range(int(-1 * N / 2), int(N / 2 + 1), 1):
     for p in range(N):
         for n in range(N):
-            F[p] += f[n] * np.exp(-2j * np.pi * p * n / N)
-        if p != 0:
-            F[p] = F[p] / D_t
+            F[p] += f[n] * np.exp(2j * np.pi * p * n / N)
+            F[p] = F[p] * D_t
         w[p] = p * 2 * np.pi / T
 
     # print(w)
@@ -22,6 +21,14 @@ def DFT(f, t):
     print('the smallest change in angular frequency that can be resolved is', 2 * np.pi / T)
     return F, w
 
+def IDFT(F):
+    N = len(F)
+    f = np.zeros(N, dtype=complex)
+    for n in range(N):
+        for p in range(N):
+            f[n] += F[p] * np.exp(-2j * np.pi * p * n / N)
+    f = f / N
+    return f
 
 def plotting(x, y, x_label='x', y_label='y'):
     plt.plot(x, y)
@@ -45,9 +52,8 @@ def find_diff_between_peaks(x, y):
 def create_gaussian(x, sigma, mu):
     # gaussian has to be created to be periodic from 0 to T
     half = int(len(x) / 2)
-    y = 1 / (sigma * np.sqrt(2 * np.pi)) * \
-        np.exp(-1 * (x - mu)**2 / (2 * sigma**2))
-    return np.array([*y[half:], *y[:half]])
+    y = 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-1 * (x - mu)**2 / (2 * sigma**2))
+    return np.concatenate([y[half:], y[:half]])
 
 
 '''
@@ -61,16 +67,16 @@ DFT(y, x)
 
 # DFT for data
 time, function = np.loadtxt('C:/Users/yx200\Desktop\imperial\yr3\computational_physics\PS3\data_6.txt', unpack=True)
-plotting(time, function, 'time', 'function')
+#plotting(time, function, 'time', 'function')
 
+############ something wrong with DFT_of_function ############
 DFT_of_function, angular_frequency = DFT(function, time)
-DFT_of_function, angular_frequency = DFT_of_function[:50], angular_frequency[:50]
+DFT_of_function_cut, angular_frequency_cut = DFT_of_function[:50], angular_frequency[:50]
 w_unit = 1 / 1e-12
 # value at 0 frequency because the function has non zero average
-plotting(angular_frequency, DFT_of_function,
-         'angular frequency, $10^{12}$ rad/s', 'DFT of function')
+plotting(angular_frequency, DFT_of_function,'angular frequency, $10^{12}$ rad/s', 'DFT of function')
 
-diff = find_diff_between_peaks(angular_frequency, DFT_of_function)
+diff = find_diff_between_peaks(angular_frequency_cut, DFT_of_function_cut)
 diff = 0.75205693e12    # diff found to be 0.75205693e12
 h_bar = 1.054571817e-34
 Energy = h_bar * diff
@@ -78,10 +84,10 @@ B_0 = Energy / (1 * (1 + 1))    # = 3.965490215787709e-23, for 1st peak
 # The diatomic molecule is Nitrogen
 
 
-# creating gussians for
-x = np.linspace(-1, 1, 1000)
+# creating gussians for (number of points chosen to match with the data)
+x = np.linspace(-max(time)/2, max(time)/2, len(time))
 y = create_gaussian(x, 0.1, 0)    # using units of picoseconds (1e-12)
-plotting(x, y, 'time, $10^{-12}$ s', 'normalised gaussian')
+#plotting(x, y, 'time, $10^{-12}$ s', 'normalised gaussian')
 DFT_of_gaussian, angular_frequency_gaussian = DFT(y, x)
 '''
 G_half = int(len(DFT_of_gaussian)/2)
@@ -92,4 +98,10 @@ plt.xlim(1500, 1650)
 plotting(angular_frequency_gaussian, DFT_of_gaussian,
          'angular frequency, $10^{12}$ rad/s', 'DFT of gaussian')
 '''
-plotting(angular_frequency_gaussian, DFT_of_gaussian, 'angular frequency, $10^{12}$ rad/s', 'DFT of gaussian')
+#plotting(angular_frequency_gaussian, DFT_of_gaussian, 'angular frequency, $10^{12}$ rad/s', 'DFT of gaussian')
+
+# convolution in time space is multiplication in frequency space
+convolved_function_in_w = DFT_of_function * DFT_of_gaussian
+#plotting(angular_frequency, convolved_function_in_w, 'angular frequency, $10^{12}$ rad/s', 'convolved function in w')
+inversed_function = IDFT(convolved_function_in_w)
+plotting(time, inversed_function, 'time, $10^{-12}$ s', 'inversed function')
