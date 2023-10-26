3f241172a93fd7d12a1694d9977558bba9d3b479
Chris-xun <yx20021225@163.com>
2023-10-26 16:38:23 +0100

    q5

--- PS2/5todo.py
+++ PS2/5todo.py
@@ -14,6 +14,28 @@ def convert_to_distance(theta1, theta2):
     theta2 = np.radians(theta2)
     return abs(theta1 - theta2) * R_E
 
+# function to invert a matrix in steps, could also just use np.linalg.inv() directly
+def invert(T):
+    det = np.linalg.det(T)
+    if det == 0:
+        raise ValueError("Matrix is not invertible")
+    T_inv = np.transpose(T)
+    T_inv = T_inv / det
+    return T_inv
+
+def invert_by_gauss_jordan_elimination(M):
+    inv = np.identity(len(M))
+    for j in range(len(M)): # loop over columns
+        # normalising each column and finding the index of the 1 or -1 rows (pivot row)
+        column_max = np.amax(np.abs(M[:,j]))
+        M[:,j], inv[:,j] = M[:,j]/column_max, inv[:,j]/column_max
+        index = np.where((M[:,j] == 1.0) | (M[:,j] == -1.0))[0][0]
+        for i in range(len(M)): # loop over rows
+            if i != index: # don't act on pivot rows
+                M[i,:] -= M[index,:] * M[i,j]
+                inv[i,:] -= inv[index,:] * M[i,j]
+    return M, inv
+
 # locations are:    HER, HBK, TSU, KMH, data stored in that order
 locations = ['HER', 'HBK', 'TSU', 'KMH']
 
@@ -27,7 +49,16 @@ KMH = pd.read_csv('C:/Users\yx200\Desktop\imperial\yr3\computational_physics\PS2
 x = np.array([0, 950, 1700])
 y = np.array([150, 1000, 0])
 
-h = 1
+# storing the inverted matrix rather than inverting it every time
+h = 1000
 xi, xj = np.meshgrid(x, x)
-T = mu * h / (h**2 + (xi + xj)**2)
-print(T)
+yi, yj = np.meshgrid(y, y)
+
+T_x = mu * h / (h**2 + (xi - xj)**2)
+T_x = np.pad(T_x, ((0,3),(0,3)) , mode='constant', constant_values=0)
+T_y = mu * h / (h**2 + (yi - yj)**2)
+T_y = np.pad(T_y, ((3,0),(3,0)) , mode='constant', constant_values=0)
+T = T_x - T_y
+print(T * 5e9)
+print(invert_by_gauss_jordan_elimination(T))
+
