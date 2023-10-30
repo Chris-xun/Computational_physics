09d1cbd793137b4ce2e62a8f09d26cc587dbcd83
Chris-xun <yx20021225@163.com>
2023-10-30 11:01:38 +0000

    q2

--- PS4/2todo.py
+++ PS4/2todo.py
@@ -1,4 +1,19 @@
 # 26.10.2023
 import numpy as np
 import matplotlib.pyplot as plt
+import random as r
 
+def y1(x):
+    return np.arccos(1 - 2 * x)
+
+def y2(x, a):
+    return a * np.sqrt(x)
+
+def y3(x):
+    return np.exp(x)
+
+def G_y1(x1, x2):
+    return np.sqrt(-2 * np.log (x1)) * np.cos(2 * np.pi * x2)
+
+def G_y2(x1, x2):
+    return np.sqrt(-2 * np.log (x1)) * np.sin(2 * np.pi * x2)
